c4bdb451b4d69d1249a69e499f58e00d75de78e4
David Michael II <knowdavidmichael@gmail.com>
2022-11-26 12:29:49 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next';
+import styles from './styles.module.scss';
 
 const Page: NextPage = () => {
   return (
-    <div>
-      <h1 style={{ fontSize: '2rem', paddingTop: '4rem' }}>Home Page</h1>
-    </div>
+    <section className={styles['home-section']}>
+      <div className={styles['column']} />
+      <div className={styles['column']}>
+        <h1 className={styles['name']}>
+          About
+          <br />
+          me
+        </h1>
+      </div>
+    </section>
   );
 };
 
