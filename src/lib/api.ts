d469cc5ba2a11028478c276265316493fd116d42
David Michael II <knowdavidmichael@gmail.com>
2022-12-02 12:21:25 +0100

    refactor: hide content api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const hygraph = new GraphQLClient(
-  'https://eu-west-2.cdn.hygraph.com/content/claxwkz2f0uta01uk2fbhen3j/master',
-);
+const HYGRAPH_CONTENT_API = process.env.HYGRAPH_API;
+
+export const hygraph = new GraphQLClient(`${HYGRAPH_CONTENT_API}`);
